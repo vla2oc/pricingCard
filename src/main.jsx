d9415139f989000ed75e1df0492d22d794cd71b3
vla2oc <kurochka265@gmail.com>
2025-04-15 15:59:45 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Header from './components/Header.jsx'
+import SiderApp from './components/SiderApp.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
